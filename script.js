8672d9c6bb0ebe3efefd66f34be1776706c67a79
M.Sankari <120647441+sankariparvath@users.noreply.github.com>
2023-01-02 05:32:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// var resume={
+//     "career objective":{
+//      'career':"to be part of company that indulges professional growth which provides challenging and rewarding career whilr allowing me to uitliz my knowlege and skills"
+//     },
+//   "personal details":
+//   {
+//     'name':"M.sankari",
+//     'father name': "N.Murugan",
+//     'mother name':"M.chithra",
+//     'date of birth': "22-01-2001",
+//     'gender':"female",
+//     'marital status':"single",
+//     'nationality':"indian",
+//     'religion':"hindu",
+//     'mobile no': 7824804291
+//   }
+// }
+// // console.log(resume["career objective"]);
+// // console.table(resume["personal details"]);
+// console.assert(resume);
+console.log("Resume");
+var head1=[ 
+{
+    
+       careerobjective : "to be a part o fo company that indulges professional growth which provides challenging and rewaarding career whilw allowing me to utilize my knowledge and skills.",
+},
+{
+summary :"i am good fun and frendly person, i am honest and punctual. i ahve good communication skill. i have creative mind and am always realy for new challenges. i can deal with difficulti situations. maybe a lot of people are more the good then me, but with perseverance and strong will can defeat all."
+},
+{
+    personalskills:"vreativity, interpersonal skills, critical thinking, problem solving, public speaking, customer service skills, teamwork skill, communication."
+}
+]
+ var second =[
+{
+    Qualification:'Bsc.computer science',
+    Schoolcollege:'Sridevi arts and science college',
+    Board:'University of medras',
+    yearofpassing:'2022',
+    percentageCGPA:'85%'
+},
+{
+    Qualification:'HSC',
+    Schoolcollege:'KBC GOVT GIRLS HR SEC school redhils',
+    Board:'State board',
+    yearofpassing:'2019',
+    percentageCGPA:'48%'
+},
+{
+    Qualification:'SSLC',
+    Schoolcollege:'govt high school n.e.v. palayam, thiruvallur',
+    Board:'State board',
+    yearofpassing:'2017',
+    percentageCGPA:'78%'
+}
+ ]
+
+ var skills=
+    ["c","c++","HTML5", "CSS3", "JAVASCRIPT", "RDBMS,", "SQL", "ms office"]
+    var personalinfo={
+name:"sankari",
+father:"N.MUrugan",
+DOB:"02-01-2002",
+Gender:"Female",
+MAritalstatus:"single",
+Nationality:"indian",
+Religion:"HIndu",
+Mobileno: 7824804291
+    }
+console.log(head1);
+console.table(second)
+console.log(skills)
+console.table(personalinfo)
